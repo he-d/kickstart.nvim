0d182bebd5e47a0c83d46a3996eaf0358d00b707
Halit Emre Dilalan <hedilalan@gmail.com>
2026-06-29 21:41:17 +0200

    rewrite custom plugins from lazy to vimpack - part2

--- init.lua
+++ init.lua
@@ -644,47 +644,47 @@ do
       -- or a suggestion from your LSP for this to activate.
       map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
-          -- Find references for the word under your cursor.
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+      -- Find references for the word under your cursor.
+      map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+      -- Jump to the implementation of the word under your cursor.
+      --  Useful when your language has ways of declaring types without an actual implementation.
+      map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+      -- Jump to the definition of the word under your cursor.
+      --  This is where a variable was first declared, or where a function is defined, etc.
+      --  To jump back, press <C-t>.
+      map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
       -- WARN: This is not Goto Definition, this is Goto Declaration.
       --  For example, in C this would take you to the header.
       map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+      -- Fuzzy find all the symbols in your current document.
+      --  Symbols are things like variables, functions, types, etc.
+      map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+      -- Fuzzy find all the symbols in your current workspace.
+      --  Similar to document symbols, except searches over your entire project.
+      map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+      -- Jump to the type of the word under your cursor.
+      --  Useful when you're not sure what type a variable is and you want to see
+      --  the definition of its *type*, not where it was *defined*.
+      map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
+      -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+      ---@param client vim.lsp.Client
+      ---@param method vim.lsp.protocol.Method
+      ---@param bufnr? integer some lsp support methods only in specific files
+      ---@return boolean
+      local function client_supports_method(client, method, bufnr)
+        if vim.fn.has 'nvim-0.11' == 1 then
+          return client:supports_method(method, bufnr)
+        else
+          return client.supports_method(method, { bufnr = bufnr })
+        end
+      end
 
       -- The following two autocommands are used to highlight references of the
       -- word under your cursor when your cursor rests there for a little while.
@@ -798,7 +798,7 @@ do
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     -- You can add other tools here that you want Mason to install
-     'stylua', -- Used to format Lua code
+    'stylua', -- Used to format Lua code
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -867,7 +867,11 @@ do
   -- require('luasnip.loaders.from_vscode').lazy_load()
 
   -- [[ Autocomplete Engine ]]
-  vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
+  vim.pack.add {
+    --{ src = 'https://github.com/folke/lazydev.nvim' },
+    { src = gh 'folke/lazydev.nvim' },
+    { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' },
+  }
   require('blink.cmp').setup {
     keymap = {
       -- 'default' (recommended) for mappings similar to built-in completions
@@ -907,30 +911,27 @@ do
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
-        menu = {
-          border = 'rounded',
-        },
-        ghost_text = {
-          enabled = true,
-        },
-        print,
+      menu = {
+        border = 'rounded',
+      },
+      ghost_text = {
+        enabled = true,
+      },
     },
 
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
-        providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-          lsp = {
-            transform_items = function(_, items)
-              for _, item in ipairs(items) do
-                if item.label.label then
-                  item.label = item.label.label
-                end
-              end
-              return items
-            end,
-          },
+      providers = {
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        lsp = {
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              if item.label.label then item.label = item.label.label end
+            end
+            return items
+          end,
         },
+      },
     },
 
     snippets = { preset = 'luasnip' },
@@ -1028,8 +1029,8 @@ do
   -- require 'kickstart.plugins.debug'
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-     require 'kickstart.plugins.autopairs'
-     require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.neo-tree'
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
