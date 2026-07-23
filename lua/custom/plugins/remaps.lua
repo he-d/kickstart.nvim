8f461fd145f40d2902a508207b37a3ec8067b675
Halit Emre Dilalan <hedilalan@gmail.com>
2026-07-23 21:45:19 +0200

    update keymaps

--- lua/custom/plugins/remaps.lua
+++ lua/custom/plugins/remaps.lua
@@ -1,9 +1,11 @@
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy to system Clipboard' })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Copy to system Clipboard' })
-vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Copy to system Clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = '[Y]ank to System Clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = '[P]aste from System Clipboard' })
+
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
 vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = 'File [E][x]plorer' })
 
@@ -12,7 +14,11 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move marked Line upwards'
 
 vim.keymap.set('n', '<leader>ga', ':silent !git add %<cr>', { desc = '[G]it [a]dd current file' })
 
-vim.keymap.set('n', '<leader>q', '"_d', { desc = 'Li[q]uidate' })
 vim.keymap.set('v', '<leader>q', '"_d', { desc = 'Li[q]uidate' })
+
+vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[R]eplace word under cursor' })
+
+-- File must be open
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { desc = 'Make file e[x]ecutable', silent = true })
 
 return {}
